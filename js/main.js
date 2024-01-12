82d15999a8b29cf2706b081e2e953860e55d51ab
alessiarabacchi <alessia.rabacchi@icloud.com>
2024-01-12 10:45:34 +0100

    changes

--- js/main.js
+++ js/main.js
@@ -7,10 +7,10 @@ for (let i = 1; i <= 100; i++) {
   if (i % 15 == 0) {
     boxText = "fizzbuzz";
     boxClass = "fizzbuzz";
-  } else if (i % 15 == 5) {
+  } else if (i % 5 == 0) {
     boxText = "buzz";
     boxClass = "buzz";
-  } else if (i % 15 == 3) {
+  } else if (i % 3 == 0) {
     boxText = "fizz";
     boxClass = "fizz";
   }
